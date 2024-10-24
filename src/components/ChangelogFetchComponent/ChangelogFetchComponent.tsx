401854560e20c71a16f929cd90047c0d6993f8e9
Stephen Reaves <sreaves@redhat.com>
2024-10-24 09:43:33 -0400

    Remove unused code
    Signed-off-by: Stephen Reaves <sreaves@redhat.com>

--- src/components/ChangelogFetchComponent/ChangelogFetchComponent.tsx
+++ src/components/ChangelogFetchComponent/ChangelogFetchComponent.tsx
@@ -4,72 +4,6 @@ import { useApi, configApiRef } from '@backstage/core-plugin-api';
 import useAsync from 'react-use/lib/useAsync';
 import { parse } from 'yaml';
 
-export const exampleChanges = {
-  "results": [
-    {
-      "commit": "cf76ba0c6762fe88978e6f13baba0f66d1bfe4a2",
-      "merged_at": "2024-10-14T12:54:58.356Z",
-      "change_types": [
-        "progressive-delivery",
-        "saas-file-promotion"
-      ],
-      "error": false,
-      "apps": [
-        "osd-fleet-manager"
-      ]
-    },
-    {
-      "commit": "4b2af42ddd6224dbadcb5ee7241f1cc383048edc",
-      "merged_at": "2024-10-14T12:48:32.806Z",
-      "change_types": [
-        "resource-rds-defaults-updates",
-        "resource-updates",
-        "namespace-external-resources-updates"
-      ],
-      "error": false,
-      "apps": [
-        "Glitchtip"
-      ]
-    },
-    {
-      "commit": "e0e8a459c47ff183b10cea9f5033231abd401c56",
-      "merged_at": "2024-10-14T12:45:27.021Z",
-      "change_types": [
-        "saas-file-promotion"
-      ],
-      "error": false,
-      "apps": [
-        "osd-fleet-manager"
-      ]
-    },
-    {
-      "commit": "fdbb5159b5e71d5d1ed2a3798d01dbd285a96108",
-      "merged_at": "2024-10-14T12:41:43.941Z",
-      "change_types": [
-        "resource-rds-defaults-updates",
-        "resource-updates",
-        "namespace-external-resources-updates"
-      ],
-      "error": false,
-      "apps": [
-        "Glitchtip"
-      ]
-    },
-    {
-      "commit": "364a564a14a7e2c8233ef40d69017597248d140e",
-      "merged_at": "2024-10-14T12:37:49.661Z",
-      "change_types": [
-        "progressive-delivery",
-        "saas-file-promotion"
-      ],
-      "error": false,
-      "apps": [
-        "assisted-installer"
-      ]
-    }
-  ]
-}
-
 type Change = {
   commit: string;
   merged_at: string; // TODO: Timezone
@@ -115,7 +49,6 @@ export const ChangelogFetchComponent = () => {
     const response = await fetch(`${config.getString('backend.baseUrl')}/api/proxy/inscope-resources/resources/yaml/configmap-change-log.yaml`)
       .catch(e => e)
       .then(resp => resp);
-    console.log(response);
     if (!response.ok) {
       // TODO: Return error
     }
@@ -128,7 +61,6 @@ export const ChangelogFetchComponent = () => {
     } catch {
       // TODO: Return error
     }
-    console.log(config_map);
 
     let change_data;
     try {
@@ -136,7 +68,6 @@ export const ChangelogFetchComponent = () => {
     } catch {
       // TODO: Return error
     }
-    console.log(change_data);
 
     return change_data.items;
   }, []);
